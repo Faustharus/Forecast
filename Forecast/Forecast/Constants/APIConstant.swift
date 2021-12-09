2018befc2b469ac4028bca669777efbbe5a506e2
Faustharus <48592115+Faustharus@users.noreply.github.com>
2021-12-09 18:50:28 +0100

    Update APIConstant.swift

--- Forecast/Forecast/Constants/APIConstant.swift
+++ Forecast/Forecast/Constants/APIConstant.swift
@@ -9,7 +9,7 @@ import Foundation
 
 enum APIConstant {
     
-    static let baseURL = "https://api.openweathermap.org/data/2.5/weather?appid=2b2f9f2c1922db10ddf8202cac7d764b&units=metric"
-    static let baseWeekURL = "https://api.openweathermap.org/data/2.5/onecall?appid=2b2f9f2c1922db10ddf8202cac7d764b&units=metric"
+    static let baseURL = "https://api.openweathermap.org/data/2.5/weather?appid=API_KEY_HERE&units=metric"
+    static let baseWeekURL = "https://api.openweathermap.org/data/2.5/onecall?appid=API_KEY_HERE&units=metric"
     
 }
